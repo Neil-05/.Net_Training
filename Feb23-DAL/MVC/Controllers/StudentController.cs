7e536652a7de3b54e99578e904c93e2b8a721d5d
Neil-05 <neilparkhe@gmail.com>
2026-02-24 22:34:08 +0530

    Added Log to MVC

--- Feb23-DAL/MVC/Controllers/StudentController.cs
+++ Feb23-DAL/MVC/Controllers/StudentController.cs
@@ -1,28 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using MVC.Data;
 using MVC.Models;
+using Microsoft.Extensions.Logging;
 
 namespace MVC.Controllers
 {
     public class StudentController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger<StudentController> _logger;
 
-        public StudentController(ApplicationDbContext context)
+
+        public StudentController(ApplicationDbContext context, ILogger<StudentController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // LIST
         public IActionResult Index()
         {
+            _logger.LogInformation("Fetchingfrom the Database....");
             var students = _context.Students.ToList();
+            _logger.LogInformation("Fetched students from the database "+ students.Count);
             return View(students);
         }
 
         // GET CREATE
         public IActionResult Create()
         {
+            _logger.LogInformation("Create Page Opened....");
             return View();
         }
 
@@ -32,11 +39,13 @@ namespace MVC.Controllers
         {
             if (ModelState.IsValid)
             {
+                _logger.LogInformation($"Adding a new student to the Database {student.FullName}");
                 _context.Students.Add(student);
                 _context.SaveChanges();
+                _logger.LogInformation($"Student Added Successfully....");
                 return RedirectToAction("Index");
             }
-
+            _logger.LogInformation("Invalid Model Submitted...");
             return View(student);
         }
     }
